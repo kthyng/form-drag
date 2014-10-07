c904769a966fd652c48ae52a5d34922914c561cc
Kristen M. Thyng <kthyng@gmail.com>
2014-10-07 10:59:21 -0500

    possibly finished the form drag calculations, next is the bottom drag

--- analysis.py
+++ analysis.py
@@ -13,6 +13,7 @@ import pdb
 import matplotlib.pyplot as plt
 import tracpy.plotting
 import tracpy.calcs
+import bisect
 
 
 def plot_domain(grid, nc, dd=65):
@@ -123,6 +124,9 @@ def run():
                 grid['km'], grid['theta_s'], grid['theta_b'], 
                 h, grid['hc'], zeta=zeta[tind,:,:], Hscale=3)
 
+    # Get layer thicknesses
+    dzr = zw[:,1:,:,:] - zw[:,:-1,:,:]
+
     # Convert transect locations from x,y space to grid index space
     xgt, ygt, dt = tracpy.tools.interpolate2d(lont, latt, grid, 'd_ll2ij')
     zgt = np.arange(rho.shape[1]) # keep output at the layer locations in grid space
@@ -136,8 +140,8 @@ def run():
     rhot = np.empty((tinds.size, zgt.size, xgt.size))
     ut = np.empty((tinds.size, zgt.size, xgt.size))
     vt = np.empty((tinds.size, zgt.size, xgt.size))
-    zwt = np.empty((tinds.size, zgt.size, xgt.size))
     zrt = np.empty((tinds.size, zgt.size, xgt.size))
+    dzrt = np.empty((tinds.size, zgt.size, xgt.size)) # layer thicknesses
     for i,tind in enumerate(tinds): # loop through time
         for k in xrange(zgt.size): # loop through vertical layers
             # have to manipulate mask for this to work out
@@ -145,41 +149,81 @@ def run():
             ind = rhotemp>100 # catch masked values
             rhotemp[ind] = 0.
             rhot[i,k,:] = ndimage.map_coordinates(rhotemp, np.array([ygt, xgt]), 
-                                    order=3, mode='nearest',cval=0.)
+                                    order=1, mode='nearest',cval=0.)
             utemp = u[i,k,:,:].data
             ind = utemp>100 # catch masked values
             utemp[ind] = 0.
             ut[i,k,:] = ndimage.map_coordinates(utemp, np.array([ygt, xgt]), 
-                                    order=3, mode='nearest',cval=0.)
+                                    order=1, mode='nearest',cval=0.)
             vtemp = v[i,k,:,:].data
             ind = vtemp>100 # catch masked values
             vtemp[ind] = 0.
             vt[i,k,:] = ndimage.map_coordinates(vtemp, np.array([ygt, xgt]), 
-                                    order=3, mode='nearest',cval=0.)
-            # these gives the zw/zr value along the transect, in depth?
-            zwt[i,k,:] = ndimage.map_coordinates(zw[i,k,:,:], np.array([ygt, xgt]), 
-                                    order=3, mode='nearest',cval=0.)
+                                    order=1, mode='nearest',cval=0.)
+            # these gives the zr value along the transect, in depth
             zrt[i,k,:] = ndimage.map_coordinates(zr[i,k,:,:], np.array([ygt, xgt]), 
-                                    order=3, mode='nearest',cval=0.)
+                                    order=1, mode='nearest',cval=0.)
+            dzrt[i,k,:] = ndimage.map_coordinates(dzr[i,k,:,:], np.array([ygt, xgt]), 
+                                    order=1, mode='nearest',cval=0.)
+
+    # Add 1000 to density values
+    rhot = rhot + 1000
 
     # don't need to loop for bathymetry
-    dhdxt = ndimage.map_coordinates(dhdx, np.array([ygt, xgt]), order=3, mode='nearest',cval=0.)
-    dhdyt = ndimage.map_coordinates(dhdy, np.array([ygt, xgt]), order=3, mode='nearest',cval=0.)
+    dhdxt = ndimage.map_coordinates(dhdx, np.array([ygt, xgt]), order=1, mode='nearest',cval=0.)
+    dhdyt = ndimage.map_coordinates(dhdy, np.array([ygt, xgt]), order=1, mode='nearest',cval=0.)
+    # depths along the transect
+    ht = ndimage.map_coordinates(h, np.array([ygt, xgt]), order=1, mode='nearest',cval=0.)
+
+    # Calculate height of bump
+    HT = ht.max()-ht.min()
+    # check the depths along the transect
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(-ht)
+    fig.show()
+    print 'the height of the bump is ', HT
 
     # Calculate along-track direction (transect goes from left to right in the domain)
     dhdt = dhdxt/np.cos(theta)
 
-    # Perform integrations: TEST UNITS
-
-    # --- Integrate along track
-    g = 9.81
-    Dalong = -g * (rhot*dhdt).sum(axis=1)*dd # common along-track component
+    # Perform integrations:
 
     # --- Integrate vertically. Integrate below and above critical depth easily, and include 
     # a weighting of the percent of depth used for layers that cross z0
+    # [t,z,x'] --> [t,x']
+    z0 = -10 # critical depth for integrating surface vs. internal components
+    Dfit = np.empty((tinds.size,xgt.size)) # internal form drag
+    Dfst = np.empty((tinds.size,xgt.size)) # surface form drag
+    # pdb.set_trace()
+    for i in xrange(tinds.size): # loop in time
+        for j in xrange(xgt.size): # loop along transect
+            iz0 = bisect.bisect(zrt[i,:,j], z0) - 1 # index just below critical depth in depth
+            # internal
+            Dfit[i,j] = (rhot[i,:iz0,j]*dzrt[i,:iz0,j]).sum(axis=0) # up to the layer below the layer with z0
+            dz = ((z0-zrt[i,iz0,j])/dzrt[i,iz0,j])*abs(z0) # fraction of layer below z0 * z0 to get in meters
+            Dfit[i,j] = Dfit[i,j] + rhot[i,iz0,j]*dz # addition of part of layer that includes z0
+            # surface
+            Dfst[i,j] = (rhot[i,iz0+1:,j]*dzrt[i,iz0+1:,j]).sum(axis=0) # the layer above the layer with z0 and above
+            dz = ((zrt[i,iz0+1,j]-z0)/dzrt[i,iz0,j])*abs(z0) # fraction of layer above z0 * z0 to get in meters
+            Dfst[i,j] = Dfst[i,j] + rhot[i,iz0,j]*dz # addition of part of layer that includes z0
+
     pdb.set_trace()
-    Dfi = Dalong # internal form drag
-    Dfs = Dalong # surface form drag
+
+    # --- Integrate along track, [t,x'] --> [t]
+    # Note that Dfi and Dfs have already been divided by W, the width of the bump
+    g = 9.81 # gravity
+    Dfi = -g * (Dfit*dhdt[np.newaxis,:].repeat(tinds.size,axis=0)).sum(axis=1)*dd
+    Dfs = -g * (Dfst*dhdt[np.newaxis,:].repeat(tinds.size,axis=0)).sum(axis=1)*dd
+
+    # Divide the form drags by the height of the bump, HT
+    Dfi = Dfi/HT
+    Dfs = Dfs/HT
+
+
+    # Calculate the bottom friction
+    # MAKE SURE UNITS ARE THE SAME IN THE END
 
 if __name__ == "__main__":
-    run()    
+    run() 
+
